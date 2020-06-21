9167a4ad219a4f9bbf2de732ef738ae844ab2e95
Boniface Mwenda <boniface.mwenda@andela.com>
2020-06-21 20:20:29 +0300

    Fix search errors

--- main.py
+++ main.py
@@ -3,22 +3,19 @@ import requests
 
 search = input("search for:")
 params = {"q":search}
-r = requests.get("http.//www.bing.com/search/", params=params)
+r = requests.get("http://www.bing.com/search/", params=params)
 
 soup = BeautifulSoup(r.text, "html.parser")
 results = soup.find("ol", {"id": "b_results"})
-links = results.findAll("li", {"class": "b-algo"})
+links = results.findAll("li", {"class": "b_algo"})
 
 for item in links:
     item_text = item.find("a").text
-    item_href = item.find("a").attrs("href")
+    item_href = item.find("a").attrs["href"]
 
     if item_text and item_href:
         print(item_text)
         print(item_href)
 
-        children = item.children("h2")
+        children = item.find("h2")
         print("next sibling of the h2:", children.next_sibling)
-
-
-
